cf70d1999d6a6f46ac1e4a9cd71a5b2799720a57
Anisur Rahman <anis.dapunia@gmail.com>
2024-07-04 07:57:04 +0600

    firebase login logout functionality added

--- src/authProvider/AuthProvider.jsx
+++ src/authProvider/AuthProvider.jsx
@@ -1,14 +1,58 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebaseConfig/FirebaseConfig";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const resetPass = (email) => {
+    setLoading(true);
+    return sendPasswordResetEmail(auth, email);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unSubscribe;
+    };
+  }, []);
   const userInfo = {
+    user,
+    loading,
+    setLoading,
     createUser,
+    loginUser,
+    googleLogin,
+    logOutUser,
+    resetPass,
   };
   return (
     <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
